a9edce58350e506fbb1c79b0d982a1d69680e348
Pinki Jha <pinkijha1310@gmail.com>
2024-12-01 01:45:52 +0530

    fixed the protfolio card overlap on navbar by adding z-index on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -39,7 +39,7 @@ const Navbar = () => {
   return (
     <>
     {/* Desktop navbar */}
-    <div className='bg-white flex items-center  max-w-screen-2xl mx-auto fixed top-0 left-0 right-0
+    <div className='z-20 bg-white flex items-center  max-w-screen-2xl mx-auto fixed top-0 left-0 right-0
      container justify-between px-4 md:px-20 shadow-md'>
       <div className='flex items-center space-x-2 cursor-pointer'>
         <img className='rounded-full h-10  md:h-14 md:m-2 ' src={photo} alt='Photo' />
