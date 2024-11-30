ee674700481a7658b53d54eaf43b3e3dedadc89c
Pinki Jha <pinkijha1310@gmail.com>
2024-12-01 02:00:54 +0530

    make some changes on portfolio components

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -13,42 +13,50 @@ const Portfolio = () => {
         {
             id: 1,
             logo: react,
-            name: 'ReactJs'
+            name: 'ReactJs',
+            projectName: 'Portfolio, DashBoard, Swiggy-Clone, Youtube-Clone, Netflix-Clone',
         },
         {
             id: 2,
             logo: js,
-            name: 'JavaScript'
+            name: 'JavaScript',
+            projectName: 'Calculators, Age-Calci, Stone-Paper-Scissor, Tic-Tac-Toe',
         },
         {
             id: 3,
             logo: css,
-            name: 'CSS'
+            name: 'CSS',
+            projectName: 'Amazone-Landing-Page',
         },
         {
             id: 4,
             logo: html,
-            name: 'HTML'
+            name: 'HTML',
+            projectName: 'Amazone-Landing-Page',
         },
         {
             id: 5,
             logo: tailwind,
-            name: 'Tailwind'
+            name: 'Tailwind',
+            projectName: 'Portfolio, DashBoard, Swiggy-Clone, Youtube-Clone, Netflix-Clone',
         },
         {
             id: 6,
             logo: boot,
-            name: 'Bootstrap'
+            name: 'Bootstrap',
+            projectName: 'Portfolio, DashBoard, Swiggy-Clone, Youtube-Clone, Netflix-Clone',
         },
         {
             id: 7,
             logo: angular,
-            name: 'Angular'
+            name: 'Angular',
+            projectName: 'Todo-List, Crud-Operation',
         },
         {
             id: 8,
             logo: node,
-            name: 'NodeJs'
+            name: 'NodeJs',
+            projectName: 'Work In Progress ',
         },
     ]
   return (
@@ -57,18 +65,18 @@ const Portfolio = () => {
         <h1 className="text-3xl font-bold mb-4 md:mb-5">PortFolio</h1>
         <spna className='font-semibold underline' >Featured Project</spna>
         <div className='grid md:grid-cols-4 grid-cols-1 gap-3 space-y-3 my-5'>
-            {cardItem.map(({id, logo, name}) => (
+            {cardItem.map(({id, logo, name, projectName}) => (
                 <div className='md:w-[300px] md:h-[300px] border rounded-lg p-2  shadow-lg
                 cursor-pointer hover:scale-90 md:hover:scale-110 duration-200  ' key={id}>
                     <img className='w-[100px] h-[100px] rounded-full p-2 ' src={logo} alt='react_logo' />
                     <div>
                         <div className='font-bold mb-2 px-2 text-md md:text-xl'>{name}</div>
-                        <p className='px-2 text-gray-700'>Lorem ipsum, , quo voluptatum asperiores deleniti quibusdam!</p>
+                        <p className='px-2 text-gray-700'>{projectName}</p>
                     </div>
 
-                    <div className='space-x-3 space-y-3 px-6 py-6 justify-around'>
-                        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold px-2 py-2 rounded'>Video</button>
-                        <button className='bg-green-500  hover:bg-green-700 text-white font-bold px-2 py-2 rounded'>Source Code</button>
+                    <div className='space-x-3 md:space-y-0 space-y-3 px-6 py-6 justify-around'>
+                        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold px-2 py-2 rounded'><a href='https://www.linkedin.com/in/pinkijha/' target='_blank'>Show</a></button>
+                        <button className='bg-green-500  hover:bg-green-700 text-white font-bold px-2 py-2 rounded'><a href='https://github.com/pinkijha' target='_blank'>Source Code</a></button>
                     </div>
 
                 </div>
